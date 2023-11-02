0ab5982e72f7ffaa1ac17057639c51a148f3d8ef
Angel Argonza <argonza_roan@outlook.com>
2023-11-02 15:35:48 -0600

    dark mode implementado

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(){
     eventListeners();
+    darkmode();
 });
+
+function darkmode(){
+    const botonDarkMode = document.querySelector('.dark-mode-button');
+    const defaultDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if(defaultDarkMode.matches){
+        document.body.classList.add('dark-mode');
+    }else{
+        document.body.classList.remove('dark-mode');
+    }
+
+    defaultDarkMode.addEventListener('change', function(){
+        if(defaultDarkMode.matches){
+            document.body.classList.add('dark-mode');
+        }else{
+            document.body.classList.remove('dark-mode');
+        }
+    });
+    botonDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode'); 
+    });
+}
 
 function eventListeners(){
     const mobileMenu = document.querySelector('.mobile-menu');
